2e9cc0a0646e200ec633c5153fedb1e275127b7c
Srikanth Bhupathi <srikanthbhupathi001@gmail.com>
2023-02-25 15:41:59 -0600

    new version

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.sql.SQLOutput;
+
+class Human{
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String a[]) {
+        Human H = new Human();
+        H.setAge(28);
+        H.setName("sri");
+        System.out.println(H.getAge() +":"+H.getName() );
     }
 }
